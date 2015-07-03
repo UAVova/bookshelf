6526c4ca1919507b6d7c40cffdf29660406a2622
Volodymyr Kulyk <dev.volodymyr@gmail.com>
2015-07-03 11:53:15 +0300

    Added show page for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,8 @@ class BooksController < ApplicationController
     @books = Book.all.paginate(:page => params[:page], :per_page => 5)
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
 end
